1e050655661e18846d12f0f6172c532bb37aefc1
David <17292606+reznik-gh@users.noreply.github.com>
2025-03-20 11:14:55 +0100

    Erstes eigenes Modul o.O

---
+++ modules/repo-installer/repo-installer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# rpmfusion free only
+echo "***** Installiere RPMfusion *****"
+dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+# Proton VPN Repo
+# dnf5 install https://repo.protonvpn.com/fedora-41-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm
