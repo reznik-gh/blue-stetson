3c4cea6b1a71f8432dde98dadf1949aad0207f83
David <17292606+reznik-gh@users.noreply.github.com>
2025-04-03 16:00:09 +0200

    Ghostty

--- modules/repo-installer/repo-installer.sh
+++ modules/repo-installer/repo-installer.sh
@@ -5,5 +5,8 @@ set -euo pipefail
 echo "***** Installiere RPMfusion *****"
 dnf5 install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
+# Ghostty
+dnf5 --assumeyes copr enable pgdev/ghostty
+
 # Proton VPN Repo
 # dnf5 install https://repo.protonvpn.com/fedora-41-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.2-1.noarch.rpm
